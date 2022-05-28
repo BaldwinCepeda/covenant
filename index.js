b44a2e5c2ed5cc04c5c4fa25ea476c1d2ec2fb88
Baldwin <Baldwincepeda@gmail.com>
2022-05-28 17:00:44 -0400

    init

---
+++ index.js
@@ -0,0 +1,39 @@
+const fetch = require('node-fetch');
+
+console.log('Getting Proposal ID...')
+
+var id = 'QmWbpCtwdLzxuLKnMW4Vv4MPFd2pdPX71YBKPasfZxqLUS'
+var spaceiD = ""
+//api call to snapshot 
+async function getSpaceID(id) {
+
+    fetch('https://hub.snapshot.org/graphql', {
+        method: 'POST',
+      
+        headers: {
+          "Content-Type": "application/json"
+        },
+      
+        body: JSON.stringify({
+          query: `query Proposal {
+            proposal(id: "` + id +`") {
+              snapshot
+              space {
+                id
+                network
+                strategies {
+                  name
+                  network
+                  params
+                }
+              }
+            }
+          }`
+        })
+      })
+      .then(res => res.json())
+      .then(res => console.log(res.data.proposal.space.id ))
+}
+
+
+var spaceID = getSpaceID("QmWbpCtwdLzxuLKnMW4Vv4MPFd2pdPX71YBKPasfZxqLUS");
